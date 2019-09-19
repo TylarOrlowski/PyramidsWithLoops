9ee9ec6581d8c46be684ec85d0836ea302026069
orlowstt <orlowstt@LCBLAB2110-04.uccob.uc.edu>
2019-09-19 09:16:18 -0400

    Finished Project

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -14,12 +14,20 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		int lines = 8;
+		for (int i = 0; i <= lines; i++) {
+			for ( int j = lines; j > i ; j--) {
+				System.out.print(" ");
 			}
-			System.out.println("");
+		for (int p = 1; p <= i; p++) {
+			System.out.print(i);
+		}
+		if (i > 1) {
+			for (int k = 1; k < i; k++) {
+				System.out.print(i);
+			}
+		}
+		System.out.println("");
 		}
 	}
 }
